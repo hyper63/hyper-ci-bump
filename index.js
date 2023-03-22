7da4f8fb229a3d98dc8c10efe21878c7c01dab4f
Tyler Hall <tyler@hyper.io>
2023-03-22 15:05:13 +0000

    refactor: to esm and revert using ncc to build action #3 #5
    ncc doesn't seem to be bundling some of the dependencies and is resulting
    in broken local imports in the `/dist` folder. So for now, we aren't using
    it. May need to use rollup or something directly to build

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 
-const core = require('@actions/core')
+import { existsSync, readFileSync } from 'node:fs'
+import * as core from '@actions/core'
+import * as globby from 'globby'
+import * as rc from 'rc'
+import * as sv from 'commit-and-tag-version'
 
-const { run } = require('./main')()
+const { run } = require('./main')({ sv, rc, core, globby, existsSync, readFileSync })
 
 run().catch(err => core.setFailed(err.message))
